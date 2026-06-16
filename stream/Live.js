267790c055e6a655ac3aaf7f4f3c5b23921c9ed4
crammer-web <mohamedyamni68@gmail.com>
2026-06-16 02:53:39 +0100

    Update Live.js

--- stream/Live.js
+++ stream/Live.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   const iframes = {
-    live1: "https://z1.depoooo.com/albaplayer/bein-2/",
-    live2: "https://z1.depoooo.com/albaplayer/bein-2/",
+    live1: "https://z1.depoooo.com/albaplayer/bein-3/?serv=3",
+    live2: "https://z1.depoooo.com/albaplayer/bein-3/?serv=3",
     live3: "https://sirtvnetwrok.one/hard/2908c7d4425d87350.html?match=4627902",
-    live4: "https://pl.kooralive.fit/albaplayer/bein4/?serv=0"
+    live4: "https://z1.depoooo.com/albaplayer/bein-3/?serv=3"
   };
 
   for (let id in iframes) {
