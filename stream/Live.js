30958154155fc488cdf480c170d84440bec62b33
crammer-web <mohamedyamni68@gmail.com>
2026-06-16 02:00:39 +0100

    Update Live.js

--- stream/Live.js
+++ stream/Live.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   const iframes = {
-    live1: "https://sirtvnetwrok.one/hard/2908c7d4425d87350.html?match=4627884",
+    live1: "https://sirtvnetwrok.one/hard/2908c7d4425d87351.html?match=4627902",
     live2: "https://player.syria-live.site/albaplayer/bein1/?serv=0",
     live3: "https://pl.kooralive.fit/albaplayer/bein3/?serv=0",
     live4: "https://pl.kooralive.fit/albaplayer/bein4/?serv=0"
