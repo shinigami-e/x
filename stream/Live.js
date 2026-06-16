8d7fd0e7602f0efb9156d2428b2cb3d7bf1649c8
crammer-web <mohamedyamni68@gmail.com>
2026-06-16 02:49:02 +0100

    Update Live.js

--- stream/Live.js
+++ stream/Live.js
@@ -2,7 +2,7 @@ window.onload = function() {
   const iframes = {
     live1: "https://z1.depoooo.com/albaplayer/bein-2/",
     live2: "https://z1.depoooo.com/albaplayer/bein-2/",
-    live3: "https://pl.kooralive.fit/albaplayer/bein3/?serv=0",
+    live3: "https://sirtvnetwrok.one/hard/2908c7d4425d87350.html?match=4627902",
     live4: "https://pl.kooralive.fit/albaplayer/bein4/?serv=0"
   };
 
